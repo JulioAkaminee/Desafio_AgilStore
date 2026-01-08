0ee0faed8157d9a7b83d9dc989624708a4912e86
JulioAkaminee <juliooakamine@gmail.com>
2026-01-08 02:15:12 -0300

    refactor: simplify price formatting in listarProdutos function

--- functions/listarProdutos.js
+++ functions/listarProdutos.js
@@ -1,19 +1,19 @@
-
 function listarProdutos(produtos) {
   if (!produtos || produtos.length === 0) {
     console.log("Não há produtos cadastrados.");
-    
     return;
+  }
+
+  function formatarPreco(valor) {
+    return `R$ ${valor.toFixed(2).replace(".", ",")}`;
   }
 
   const produtosFormatados = produtos.map(p => ({
     ...p,
-    preco: new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(p.preco)
-  
+    preco: formatarPreco(p.preco)
   }));
 
   console.table(produtosFormatados);
-
 }
 
 module.exports = listarProdutos;
